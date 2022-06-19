6e65e0ed44647a0ebb6c5b06fea91067559db5cf
ptr-cln <p.celiento99@gmail.com>
2022-06-19 16:32:16 +0200

    Initial Commit

---
+++ src/main/java/com/springboot/crud/app/api/PersonController.java
@@ -0,0 +1,51 @@
+package com.springboot.crud.app.api;
+
+import com.springboot.crud.app.model.Person;
+import com.springboot.crud.app.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("api/v1/person")
+@RestController
+public class PersonController {
+
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController( PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    public void addPerson(@RequestBody @NonNull Person person){
+        personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople(){
+        return personService.getAllPeople();
+    }
+
+    @GetMapping(path = "{id}")
+    public Person getPersonById(@PathVariable("id") UUID id){
+        return personService.getPersonById(id).orElse(null);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deletePersonById(@PathVariable("id") UUID id){
+        personService.deletePerson(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public void updatePerson(@PathVariable("id") UUID id,
+                             @RequestBody @NonNull Person personToUpdate){
+        personService.updatePerson(id,personToUpdate);
+    }
+
+
+}
